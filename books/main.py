192abb517e23960428c5b21fb7459ff9e4aa0014
naruto4282 <bradford@3082@gmail.com>
2024-11-14 15:06:47 -0800

    C: changed so read file and count words are both functions

--- books/main.py
+++ books/main.py
@@ -1,16 +1,19 @@
-
-
+def main():
+    
+    book_location="frankenstien.txt"
+    text = read_book(book_location)
+    num_of_words=count_words(text)
+    print("{} number of words in this file{}".format(num_of_words,book_location))
 
 def count_words(book):
     import string
     word_count=0
     book=book.split()
     word_count=len(book)
-    
-   
-    
     return word_count
 
-with open("frankenstien.txt") as f:
-    file_contents = f.read()
-    print(count_words(file_contents))
+def read_book(path):
+    with open(path) as f:
+        file_contents = f.read()
+        return file_contents
+main()
