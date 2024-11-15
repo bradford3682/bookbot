24ce514276650d5f60187d129bc7bd643ca5cdd1
naruto4282 <bradford@3082@gmail.com>
2024-11-14 17:19:34 -0800

    E: fixed code formating

--- books/main.py
+++ books/main.py
@@ -1,16 +1,15 @@
 def main():
     book_location="frankenstien.txt"
     text = read_book(book_location)
-    
     characters=count_characters(text)
     num_of_words=count_words(text)
     print("{} number of words in this file{}".format(num_of_words,book_location))
     print_character_count(characters)
+
 def count_characters(book):
     import string
     character_count=0
     characters ={}
-    
     book=str(book)
     lowered_string= book.lower()
     for word in lowered_string:
@@ -18,7 +17,6 @@ def count_characters(book):
             characters[word]+=1
         elif word.isalpha():
             characters[word]=1
-
     return characters
             
 def print_character_count(characters):
